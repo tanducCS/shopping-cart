28643a5d3619eece84eedf0f77e109a267a26389
tanducCS <99661719+tanducCS@users.noreply.github.com>
2023-09-29 11:44:25 +0700

    update fetch data from backend

--- app/page.js
+++ app/page.js
@@ -2,12 +2,31 @@
 import { StoreProvider } from '@/redux/StoreProvider';
 import Image from 'next/image'
 import React,{useState,useEffect} from 'react'
+import axios from 'axios';
 
 export default function Home() {
 
   const [cart, setCart] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get('https://backend-shopping-cart.onrender.com/products');
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    // Fetch products from the API
+    fetchProducts().then((data) => {
+      setProducts(data); // Assuming that the API returns an object with a 'shoes' property containing the product data
+    });
+  }, []);
 
   useEffect(() => {
     // Tính tổng giá trị đơn hàng và cập nhật state
@@ -93,93 +112,9 @@ export default function Home() {
     }
   };
    
+  
 
-  const data = {
-    shoes: [
-      {
-        "id": 1,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
-        "name": "Nike Air Zoom Pegasus 36",
-        "description": "The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh that targets breathability across high-heat areas. A slimmer heel collar and tongue reduce bulk, while exposed cables give you a snug fit at higher speeds.",
-        "price": 108.97,
-        "color": "#e1e7ed"
-      },
-      {
-        "id": 2,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-shield-mens-running-shoe-24FBGb__1_-removebg-preview.png",
-        "name": "Nike Air Zoom Pegasus 36 Shield",
-        "description": "The Nike Air Zoom Pegasus 36 Shield gets updated to conquer wet routes. A water-repellent upper combines with an outsole that helps create grip on wet surfaces, letting you run in confidence despite the weather.",
-        "price": 89.97,
-        "color": "#4D317F"
-      },
-      {
-        "id": 3,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/cruzrone-unisex-shoe-T2rRwS-removebg-preview.png",
-        "name": "Nike CruzrOne",
-        "description": "Designed for steady, easy-paced movement, the Nike CruzrOne keeps you going. Its rocker-shaped sole and plush, lightweight cushioning let you move naturally and comfortably. The padded collar is lined with soft wool, adding luxury to every step, while mesh details let your foot breathe. There’s no finish line—there’s only you, one step after the next.",
-        "price": 100.97,
-        "color": "#E8D026"
-      },
-      {
-        "id": 4,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/epic-react-flyknit-2-mens-running-shoe-2S0Cn1-removebg-preview.png",
-        "name": "Nike Epic React Flyknit 2",
-        "description": "The Nike Epic React Flyknit 2 takes a step up from its predecessor with smooth, lightweight performance and a bold look. An updated Flyknit upper conforms to your foot with a minimal, supportive design. Underfoot, durable Nike React technology defies the odds by being both soft and responsive, for comfort that lasts as long as you can run.",
-        "price": 89.97,
-        "color": "#FD584A"
-      },
-      {
-        "id": 5,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/odyssey-react-flyknit-2-mens-running-shoe-T3VG7N-removebg-preview.png",
-        "name": "Nike Odyssey React Flyknit 2",
-        "description": "The Nike Odyssey React Flyknit 2 provides a strategic combination of lightweight Flyknit construction and synthetic material for support. Underfoot, Nike React cushioning delivers a soft, springy ride for a route that begs to be crushed.",
-        "price": 71.97,
-        "color": "#D4D7D6"
-      },
-      {
-        "id": 6,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/react-infinity-run-flyknit-mens-running-shoe-RQ484B__2_-removebg-preview.png",
-        "name": "Nike React Infinity Run Flyknit",
-        "description": "A pioneer in the running shoe frontier honors the original pioneer of running culture with the Nike React Infinity Run Flyknit. Blue Ribbon Track Club-inspired details pay homage to the haven that was created before running was even popular. This running shoe is designed to help reduce injury and keep you on the run. More foam and improved upper details provide a secure and cushioned feel.",
-        "price": 160.0,
-        "color": "#F2F5F4"
-      },
-      {
-        "id": 7,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/react-miler-mens-running-shoe-DgF6nr-removebg-preview.png",
-        "name": "Nike React Miler",
-        "description": "The Nike React Miler gives you trusted stability for miles with athlete-informed performance. Made for dependability on your long runs, its intuitive design offers a locked-in fit and a durable feel.",
-        "price": 130.0,
-        "color": "#22AFDC"
-      },
-      {
-        "id": 8,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/renew-ride-mens-running-shoe-JkhdfR-removebg-preview.png",
-        "name": "Nike Renew Ride",
-        "description": "The Nike Renew Ride helps keep the committed runner moving with plush cushioning. Firm support at the outsole helps you maintain stability no matter the distance.",
-        "price": 60.97,
-        "color": "#B50320"
-      },
-      {
-        "id": 9,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/vaporfly-4-flyknit-running-shoe-v7G3FB-removebg-preview.png",
-        "name": "Nike Vaporfly 4% Flyknit",
-        "description": "Built to meet the exacting needs of world-class marathoners, Nike Vaporfly 4% Flyknit is designed for record-breaking speed. The Flyknit upper delivers breathable support, while the responsive foam and full-length plate provide incredible energy return for all 26.2.",
-        "price": 187.97,
-        "color": "#3569A1"
-      },
-      {
-        "id": 10,
-        "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/zoom-fly-3-premium-mens-running-shoe-XhzpPH-removebg-preview.png",
-        "name": "Nike Zoom Fly 3 Premium",
-        "description": "Inspired by the Vaporfly, the Nike Zoom Fly 3 Premium gives distance runners race-day comfort and durability. The power of a carbon fiber plate keeps you in the running mile after mile.",
-        "price": 160.0,
-        "color": "#54D4C9"
-      }
-    ]
-  }
-
-  const products = data.shoes
+  
   
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-32 bg-gradient-to-r from-purple-500 to-pink-500" >
@@ -191,7 +126,7 @@ export default function Home() {
             </div>
             <div className='listProducts mx-10 h-4/5 overflow-scroll no-scrollbar'>
             {
-              products.map((product)=>(
+              products?.map((product)=>(
                 <div key={product.id} className='mb-20' >
                     <img src={product.image} alt='img' style={{backgroundColor: product.color}} className='rounded-3xl mb-4' />
                     <h4 className='font-bold text-xl mb-4'>{product.name}</h4>
